475fe8fe306afa70d2297cce8b1596d9c6d090d7
임태혁 선임/Mobile기술팀 <ith13579@lgcns.com>
2022-11-14 13:36:22 +0900

    refactor : 라우터 컴포넌트 별 코드 스플리팅

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import './App.css'
-import ListPage from './pages/ListPage/index'
-import ViewPage from './pages/ViewPage/index'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+
+const ListPage = React.lazy(() => import("./pages/ListPage/index"));
+const ViewPage = React.lazy(() => import("./pages/ViewPage/index"));
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" component={ListPage} exact />
-        <Route path="/view/:id" component={ViewPage} exact />
+        <React.Suspense fallback={<div>로딩 중...</div>}>
+          <Route path="/" component={ListPage} exact />
+          <Route path="/view/:id" component={ViewPage} exact />
+        </React.Suspense>
       </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
